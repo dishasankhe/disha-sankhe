b6f1f1a5ca571e44b8f76fa618231540937ae932
dishasankhe <64309400+dishasankhe@users.noreply.github.com>
2020-11-18 19:49:31 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="button.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+		
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("images/6.jpg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+</head>
+<body >
+<h1 class="alert alert-success rounded-0"><small class="float-right text-muted" style="font-size: 12pt;"><kbd>Presented by:DISHA SANKHE</kbd></small></h1>
+<br>
+
+	<div id="header">
+       <br>
+	   <br>
+	   <br>
+	   <br>
+	   <br>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#ff1493;text-shadow: 2px 2px black;"> SPARKS BANK </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="btn" type="button" href="getdetail.php">View users</button>
+                </a>
+                </tr>
+				
+			   <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="btn" type="button">All Transaction Details</button>
+               </a>
+               
+               </tr>
+
+            </table>
+    </div>
+</body>
+</html>
